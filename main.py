edebacac659bc27db4702b53acb886da0be92b52
IKrehan <IrvigKrehan@Hotmail.com>
2019-09-09 03:08:15 -0300

    BiologiaQuiz

--- main.py
+++ main.py
@@ -6,59 +6,61 @@ from random import randint, shuffle
 
 
 # Click receive the answer text and compare with answer in data
-def click(answer):
+def click(answer_opt):
         global texts, use
         global button1, button2, button3, button4
-        print(answer)
 
         def go_ahead(event):
 
-                global a, b, c, d, e, f, use, img
+                global a, b, c, d, e, f, g, use, img, actual_question
 
-                # add 6 (each question has six atribbutes) to every variable used in list calls, so the next questions and answers will come
+                # add 7 (each question has seven atribbutes) to every variable used in list calls, so the next questions and answers will come
                 if use == True:
-                   a += 6
-                   b += 6
-                   c += 6
-                   d += 6
-                   e += 6
-                   f += 6
+                   a += 7
+                   b += 7
+                   c += 7
+                   d += 7
+                   e += 7
+                   f += 7
+                   g += 7
 
                 # delete the labels used to inform the user that he got it right
                 left.delete(correct_label)
                 left.delete(correct_label_border)
                 left.delete(info_label)
 
+                if use == True:
+                        try:
+                                left.itemconfig(questiontext, text=texts[b])
+                                button1.configure(text = texts[c])
+                                button2.configure(text = texts[d])
+                                button3.configure(text = texts[e])
+                                button4.configure(text = texts[f])
 
-                try:
-                        positions = [a,c,d,e]
-                        shuffle(positions)
+                                img = PhotoImage(file=texts[g])
+                                left.itemconfig(image, image=img)
 
-                        left.itemconfig(questiontext, text=texts[b])
-                        button1.configure(text = texts[positions[0]])
-                        button2.configure(text = texts[positions[1]])
-                        button3.configure(text = texts[positions[2]])
-                        button4.configure(text = texts[positions[3]])
+                                actual_question += 1
+                                left.itemconfig(counter, text=f'Pergunta {actual_question}/{total_questions}')
 
-                        img = PhotoImage(file=texts[f])
-                        left.itemconfig(image, image=img)
-                        left.update()
+                                left.update()
 
-                        use = False
+                                use = False
+                                
 
 
-                # if the list options is over the game will end
-                except:
-                        left.create_text(250, 250, text="Fim!\nObrigado por jogar <3", font=("Chilanka", 31, "bold"), fill="black", justify='center')
-                        left.create_text(250, 250, text="Fim!\nObrigado por jogar <3", font=("Chilanka", 30, "bold"), fill="red", justify='center')
+                        # if the list  has no  more items the game will end
+                        except:
+                                left.create_text(250, 250, text="Fim!\nObrigado por jogar <3", font=("Chilanka", 31, "bold"), fill="black", justify='center')
+                                left.create_text(250, 250, text="Fim!\nObrigado por jogar <3", font=("Chilanka", 30, "bold"), fill="red", justify='center')
 
 
         if use == False:
 
                 # Check each button possibility (certainly a bad way to do that)
-                if 'button1' == answer and button1['text'] == texts[a]:
+
+                if 'button1' == answer_opt and texts[c] == texts[a]:
                         use = True
-                        print(button1['text'])
 
                         correct_label = left.create_text(250, 110, text="Correto!", font=("Chilanka", 45, "bold"), fill="black")
                         correct_label_border = left.create_text(250, 110, text="Correto!", font=("Chilanka", 47, "bold"), fill="green2")
@@ -66,9 +68,8 @@ def click(answer):
 
                         root.bind("<Button-1>", go_ahead)
                 
-                if 'button2' == answer and button2['text'] == texts[a]:
+                elif 'button2' == answer_opt and texts[d] == texts[a]:
                         use = True
-                        print(button2['text'])
 
                         correct_label = left.create_text(250, 110, text="Correto!", font=("Chilanka", 45, "bold"), fill="black")
                         correct_label_border = left.create_text(250, 110, text="Correto!", font=("Chilanka", 47, "bold"), fill="green2")
@@ -76,9 +77,8 @@ def click(answer):
 
                         root.bind("<Button-1>", go_ahead)
                 
-                if 'button3' == answer and button3['text'] == texts[a]:
+                elif 'button3' == answer_opt and texts[e] == texts[a]:
                         use = True
-                        print(button3['text'])
 
                         correct_label = left.create_text(250, 110, text="Correto!", font=("Chilanka", 45, "bold"), fill="black")
                         correct_label_border = left.create_text(250, 110, text="Correto!", font=("Chilanka", 47, "bold"), fill="green2")
@@ -86,9 +86,8 @@ def click(answer):
 
                         root.bind("<Button-1>", go_ahead)
                 
-                if 'button4' == answer and button4['text'] == texts[a]:
+                elif 'button4' == answer_opt and texts[f] == texts[a]:
                         use = True
-                        print(button4['text'])
 
                         correct_label = left.create_text(250, 110, text="Correto!", font=("Chilanka", 45, "bold"), fill="black")
                         correct_label_border = left.create_text(250, 110, text="Correto!", font=("Chilanka", 47, "bold"), fill="green2")
@@ -112,6 +111,7 @@ c = 2
 d = 3
 e = 4
 f = 5
+g = 6
 
 
 right = Canvas(root, width=500, height=500, bg='SteelBlue3', highlightthickness=0)
@@ -130,26 +130,24 @@ data_file.close()
 
 texts = data.split(",")
 texts = [s.replace('\n', '') for s in texts]
-
-positions = [a,c,d,e]
-shuffle(positions)
+texts = [s.replace('|', '\n') for s in texts]
 
 question = texts[b]
-opt1_text = texts[positions[0]]
-opt2_text = texts[positions[1]]
-opt3_text = texts[positions[2]]
-opt4_text = texts[positions[3]]
+opt1_text = texts[c]
+opt2_text = texts[d]
+opt3_text = texts[e]
+opt4_text = texts[f]
 
 # Head: Tell the user the actual level and how much levels are.
-total_questions = 7
+total_questions = int(len(texts)/7)
 actual_question = 1
 
-left.create_text(250, 50, text=f'Pergunta {actual_question}/{total_questions}', font=('Chilanka', 17, 'bold'), fill='VioletRed2')
+counter = left.create_text(250, 50, text=f'Pergunta {actual_question}/{total_questions}', font=('Chilanka', 17, 'bold'), fill='VioletRed2')
 left.create_line(170, 68, 330, 68,  fill='VioletRed2')
 
 # Question: A image ilustrate it.
 
-img = PhotoImage(file=texts[5])
+img = PhotoImage(file=texts[6])
 
 image = left.create_image(250, 250, image=img)
 
